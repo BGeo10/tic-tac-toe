5439e2871244863d1f3043b3ccfa6f98051e097c
Bryan Gonzalez <bgeo10@users.noreply.github.com>
2016-11-16 15:02:41 -0800

    Created First 3 Shapes

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,26 +2,44 @@
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
 
-var currentShape = "circle"
-function SquareChange() {
+var topleftClicked = 0
+function SquareClick() {
   var canvas = document.getElementById("game-board")
-    if (currentShape == "circle") {
-      var circle = document.createElementNS(namespace, "circle")
-      circle.setAttribute("cx", 63)
-      circle.setAttribute("cy", 63)
-      circle.setAttribute("r", 20)
-      circle.setAttribute("fill", "blue")
-      canvas.appendChild(circle)
-      currentShape = "line"
-    } else {
-      var line = document.createElementNS(namespace, "line")
-      line.setAttribute("x1", 25)
-      line.setAttribute("y1", 50)
-      line.setAttribute("x2", 75)
-      line.setAttribute("y2", 100)
-      line.setAttribute("stroke-width", 10)
-      line.setAttribute("stroke", "purple")
-      canvas.appendChild(line)
-      currentShape = "circle"
-    }
+  if (topleftClicked == 0) {
+    var circle = document.createElementNS(namespace, "circle")
+    circle.setAttribute("cx", 62)
+    circle.setAttribute("cy", 63)
+    circle.setAttribute("r", 20)
+    circle.setAttribute("fill", "blue")
+    canvas.appendChild(circle)
+    topleftClicked = 1
+  }
+}
+
+var topmiddleClicked = 0
+function SquareClick2() {
+  var canvas = document.getElementById("game-board")
+  if (topmiddleClicked == 0) {
+    var circle = document.createElementNS(namespace, "circle")
+    circle.setAttribute("cx", 162)
+    circle.setAttribute("cy", 63)
+    circle.setAttribute("r", 20)
+    circle.setAttribute("fill", "blue")
+    canvas.appendChild(circle)
+    topmiddleClicked = 1
+  }
+}
+
+var toprightClicked = 0
+function SquareClick3() {
+  var canvas = document.getElementById("game-board")
+  if (toprightClicked == 0) {
+    var circle = document.createElementNS(namespace, "circle")
+    circle.setAttribute("cx", 262)
+    circle.setAttribute("cy", 63)
+    circle.setAttribute("r", 20)
+    circle.setAttribute("fill", "blue")
+    canvas.appendChild(circle)
+    toprightClicked = 1
+  }
 }
