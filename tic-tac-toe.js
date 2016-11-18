c941e4837955815225f6d4d3b9c7f5dd83c0bdc6
Bryan Gonzalez <bgeo10@users.noreply.github.com>
2016-11-18 15:03:48 -0800

    Tic-Tac-Toe

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,44 +2,246 @@
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
 
+var turn="Player1"
 var topleftClicked = 0
 function SquareClick() {
   var canvas = document.getElementById("game-board")
-  if (topleftClicked == 0) {
-    var circle = document.createElementNS(namespace, "circle")
-    circle.setAttribute("cx", 62)
-    circle.setAttribute("cy", 63)
-    circle.setAttribute("r", 20)
-    circle.setAttribute("fill", "blue")
-    canvas.appendChild(circle)
-    topleftClicked = 1
+  if(topleftClicked == 0) {
+     if (turn == "Player1") {
+       var circle = document.createElementNS(namespace, "circle")
+       circle.setAttribute("cx", 62)
+       circle.setAttribute("cy", 63)
+       circle.setAttribute("r", 20)
+       circle.setAttribute("fill", "blue")
+       canvas.appendChild(circle)
+       topleftClicked = 1
+       turn="Player2"
+    } else {
+      var rect = document.createElementNS(namespace, "rect")
+      rect.setAttribute("x", 42)
+      rect.setAttribute("y", 43)
+      rect.setAttribute("height", 40)
+      rect.setAttribute("width", 40)
+      rect.setAttribute("fill", "red")
+      canvas.appendChild(rect)
+      turn="Player1"
+      topleftClicked = 1
+    }
   }
 }
 
 var topmiddleClicked = 0
 function SquareClick2() {
   var canvas = document.getElementById("game-board")
-  if (topmiddleClicked == 0) {
-    var circle = document.createElementNS(namespace, "circle")
-    circle.setAttribute("cx", 162)
-    circle.setAttribute("cy", 63)
-    circle.setAttribute("r", 20)
-    circle.setAttribute("fill", "blue")
-    canvas.appendChild(circle)
-    topmiddleClicked = 1
+  if(topmiddleClicked == 0) {
+     if (turn == "Player1") {
+       var circle = document.createElementNS(namespace, "circle")
+       circle.setAttribute("cx", 162)
+       circle.setAttribute("cy", 63)
+       circle.setAttribute("r", 20)
+       circle.setAttribute("fill", "blue")
+       canvas.appendChild(circle)
+       topmiddleClicked = 1
+       turn="Player2"
+    } else {
+      var rect = document.createElementNS(namespace, "rect")
+      rect.setAttribute("x", 142)
+      rect.setAttribute("y", 43)
+      rect.setAttribute("height", 40)
+      rect.setAttribute("width", 40)
+      rect.setAttribute("fill", "red")
+      canvas.appendChild(rect)
+      turn="Player1"
+      topmiddleClicked = 1
+    }
   }
 }
 
 var toprightClicked = 0
 function SquareClick3() {
   var canvas = document.getElementById("game-board")
-  if (toprightClicked == 0) {
-    var circle = document.createElementNS(namespace, "circle")
-    circle.setAttribute("cx", 262)
-    circle.setAttribute("cy", 63)
-    circle.setAttribute("r", 20)
-    circle.setAttribute("fill", "blue")
-    canvas.appendChild(circle)
-    toprightClicked = 1
+  if(toprightClicked == 0) {
+     if (turn == "Player1") {
+       var circle = document.createElementNS(namespace, "circle")
+       circle.setAttribute("cx", 262)
+       circle.setAttribute("cy", 63)
+       circle.setAttribute("r", 20)
+       circle.setAttribute("fill", "blue")
+       canvas.appendChild(circle)
+       toprightClicked = 1
+       turn="Player2"
+    } else {
+      var rect = document.createElementNS(namespace, "rect")
+      rect.setAttribute("x", 242)
+      rect.setAttribute("y", 43)
+      rect.setAttribute("height", 40)
+      rect.setAttribute("width", 40)
+      rect.setAttribute("fill", "red")
+      canvas.appendChild(rect)
+      turn="Player1"
+      toprightClicked = 1
+    }
+  }
+}
+
+var middleleftClicked = 0
+function SquareClick4() {
+  var canvas = document.getElementById("game-board")
+  if(middleleftClicked == 0) {
+     if (turn == "Player1") {
+       var circle = document.createElementNS(namespace, "circle")
+       circle.setAttribute("cx", 62)
+       circle.setAttribute("cy", 163)
+       circle.setAttribute("r", 20)
+       circle.setAttribute("fill", "blue")
+       canvas.appendChild(circle)
+       middleleftClicked = 1
+       turn="Player2"
+    } else {
+      var rect = document.createElementNS(namespace, "rect")
+      rect.setAttribute("x", 42)
+      rect.setAttribute("y", 143)
+      rect.setAttribute("height", 40)
+      rect.setAttribute("width", 40)
+      rect.setAttribute("fill", "red")
+      canvas.appendChild(rect)
+      turn="Player1"
+      middleleftClicked = 1
+    }
+  }
+}
+
+var centermiddleClicked = 0
+function SquareClick5() {
+  var canvas = document.getElementById("game-board")
+  if(centermiddleClicked == 0) {
+     if (turn == "Player1") {
+       var circle = document.createElementNS(namespace, "circle")
+       circle.setAttribute("cx", 162)
+       circle.setAttribute("cy", 163)
+       circle.setAttribute("r", 20)
+       circle.setAttribute("fill", "blue")
+       canvas.appendChild(circle)
+       centermiddleClicked = 1
+       turn="Player2"
+    } else {
+      var rect = document.createElementNS(namespace, "rect")
+      rect.setAttribute("x", 142)
+      rect.setAttribute("y", 143)
+      rect.setAttribute("height", 40)
+      rect.setAttribute("width", 40)
+      rect.setAttribute("fill", "red")
+      canvas.appendChild(rect)
+      turn="Player1"
+      centermiddleClicked = 1
+    }
+  }
+}
+
+var middlerightClicked = 0
+function SquareClick6() {
+  var canvas = document.getElementById("game-board")
+  if(middlerightClicked == 0) {
+     if (turn == "Player1") {
+       var circle = document.createElementNS(namespace, "circle")
+       circle.setAttribute("cx", 262)
+       circle.setAttribute("cy", 163)
+       circle.setAttribute("r", 20)
+       circle.setAttribute("fill", "blue")
+       canvas.appendChild(circle)
+       middlerightClicked = 1
+       turn="Player2"
+    } else {
+      var rect = document.createElementNS(namespace, "rect")
+      rect.setAttribute("x", 242)
+      rect.setAttribute("y", 143)
+      rect.setAttribute("height", 40)
+      rect.setAttribute("width", 40)
+      rect.setAttribute("fill", "red")
+      canvas.appendChild(rect)
+      turn="Player1"
+      middlerightClicked = 1
+    }
+  }
+}
+
+var bottomleftClicked = 0
+function SquareClick7() {
+  var canvas = document.getElementById("game-board")
+  if(bottomleftClicked == 0) {
+     if (turn == "Player1") {
+       var circle = document.createElementNS(namespace, "circle")
+       circle.setAttribute("cx", 62)
+       circle.setAttribute("cy", 263)
+       circle.setAttribute("r", 20)
+       circle.setAttribute("fill", "blue")
+       canvas.appendChild(circle)
+       bottomleftClicked = 1
+       turn="Player2"
+    } else {
+      var rect = document.createElementNS(namespace, "rect")
+      rect.setAttribute("x", 42)
+      rect.setAttribute("y", 243)
+      rect.setAttribute("height", 40)
+      rect.setAttribute("width", 40)
+      rect.setAttribute("fill", "red")
+      canvas.appendChild(rect)
+      turn="Player1"
+      bottomleftClicked = 1
+    }
+  }
+}
+
+var bottommiddleClicked = 0
+function SquareClick8() {
+  var canvas = document.getElementById("game-board")
+  if(bottommiddleClicked == 0) {
+     if (turn == "Player1") {
+       var circle = document.createElementNS(namespace, "circle")
+       circle.setAttribute("cx", 162)
+       circle.setAttribute("cy", 263)
+       circle.setAttribute("r", 20)
+       circle.setAttribute("fill", "blue")
+       canvas.appendChild(circle)
+       bottommiddleClicked = 1
+       turn="Player2"
+    } else {
+      var rect = document.createElementNS(namespace, "rect")
+      rect.setAttribute("x", 142)
+      rect.setAttribute("y", 243)
+      rect.setAttribute("height", 40)
+      rect.setAttribute("width", 40)
+      rect.setAttribute("fill", "red")
+      canvas.appendChild(rect)
+      turn="Player1"
+      bottommiddleClicked = 1
+    }
+  }
+}
+
+var bottomrightClicked = 0
+function SquareClick9() {
+  var canvas = document.getElementById("game-board")
+  if(bottomrightClicked == 0) {
+     if (turn == "Player1") {
+       var circle = document.createElementNS(namespace, "circle")
+       circle.setAttribute("cx", 262)
+       circle.setAttribute("cy", 263)
+       circle.setAttribute("r", 20)
+       circle.setAttribute("fill", "blue")
+       canvas.appendChild(circle)
+       bottomrightClicked = 1
+       turn="Player2"
+    } else {
+      var rect = document.createElementNS(namespace, "rect")
+      rect.setAttribute("x", 242)
+      rect.setAttribute("y", 243)
+      rect.setAttribute("height", 40)
+      rect.setAttribute("width", 40)
+      rect.setAttribute("fill", "red")
+      canvas.appendChild(rect)
+      turn="Player1"
+      bottomrightClicked = 1
+    }
   }
 }
